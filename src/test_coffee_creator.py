ad5ca96c3998a4a6562ce656f9195fa4132f5bc2
Laurent Bourgon <laurent.bourgon@outlook.com>
2020-06-23 16:26:38 -0400

    added coffee_creator tests

---
+++ src/test_coffee_creator.py
@@ -0,0 +1,40 @@
+import unittest
+from coffee_creator import Coffee
+
+class TestCoffeeCreator(unittest.TestCase):
+    coffee = Coffee('Coffee1', {'water': 30, 'milk': '25', 'cocoa': 5}, 6.00)
+
+    def test_init(self):
+        # Test if conversions to string and ints was correct
+        self.assertEqual(self.coffee.name, 'Coffee1')
+        self.assertEqual(self.coffee.ingredients, {'water': 30, 'cocoa': 5})
+        self.assertEqual(self.coffee.price, 6)
+    
+    def test_final_cost(self):
+        # Test if integer works
+        self.assertEqual(self.coffee.final_cost(2), 6*2)
+        # Test if float works
+        self.assertEqual(self.coffee.final_cost(3.5), 6*3)
+    
+    def test_ingredients_needed(self):
+        # Test if integer works
+        self.assertEqual(self.coffee.ingredients_needed(2), {'water': 30*2, 'cocoa': 5*2})
+        # Test if float works
+        self.assertEqual(self.coffee.ingredients_needed(3.5), {'water': 30*3, 'cocoa': 5*3})
+    
+    def test_coffees_possible(self):
+        # Test if integer works
+        self.assertEqual(self.coffee.coffees_possible_ingredient('water', 30*2), 2)
+        self.assertEqual(self.coffee.coffees_possible_ingredient('cocoa', 5*2), 2)
+        # Test if float works
+        self.assertEqual(self.coffee.coffees_possible_ingredient('water', 30*3.5), 3)
+    
+    def test_limiting_ingredients(self):
+        # Test normally
+        self.assertEqual(self.coffee.limiting_ingredient({'water': 30, 'cocoa': 30}), {1: {'water'}})
+        # Test if one is under
+        self.assertEqual(self.coffee.limiting_ingredient({'water': 30, 'cocoa': 0}), {0: {'cocoa'}})
+        # Check if adding something not in the recipe works
+        self.assertEqual(self.coffee.limiting_ingredient({'water': 30, 'milk': 0, 'cocoa': 50}), {1: {'water'}})
+        # Check if balanced recipe works
+        self.assertEqual(self.coffee.limiting_ingredient({'water': 30, 'milk': 0, 'cocoa': 5}), {1: {'water', 'cocoa'}})

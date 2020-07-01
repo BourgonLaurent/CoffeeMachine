628975d7a13df4f1fc23a039a6429b797c031a22
Laurent Bourgon <laurent.bourgon@outlook.com>
2020-07-01 17:32:57 -0400

    formatting

--- src/coffee_creator.py
+++ src/coffee_creator.py
@@ -1,6 +1,7 @@
 ### coffee_creator.py
 ## CREATE A COFFEE
 
+
 class Coffee:
     """## Basic Coffee Class
         
@@ -10,11 +11,15 @@ class Coffee:
         \tprice {int} -- Price of the coffee
     """
 
-    def __init__(self, name: str, ingredients: dict, price: int):   
+    def __init__(self, name: str, ingredients: dict, price: int):
         self.name = str(name)
-        self.ingredients = {ingredient: int(amount) for ingredient, amount in ingredients.items() if isinstance(amount, int)}
+        self.ingredients = {
+            ingredient: int(amount)
+            for ingredient, amount in ingredients.items()
+            if isinstance(amount, int)
+        }
         self.price = int(price)
-    
+
     def __str__(self):
         return f"{self.name} - {self.price}$:\t{' | '.join([f'{ingredient} - {amount} mL' for ingredient, amount in self.ingredients.items()])}"
 
@@ -30,15 +35,18 @@ class CoffeeCreator(Coffee):
 
     def __init__(self, name: str, ingredients: dict, price: int):
         super().__init__(name, ingredients, price)
-    
+
     def final_cost(self, number_of_coffees: int):
         """Return the total price of the number of coffees specified"""
         return self.price * int(number_of_coffees)
-    
+
     def ingredients_needed(self, number_of_coffees: int):
         """Return how many ingredients will be needed for the number of coffees specified"""
-        return {ingredient: amount * int(number_of_coffees) for ingredient, amount in self.ingredients.items()}
-    
+        return {
+            ingredient: amount * int(number_of_coffees)
+            for ingredient, amount in self.ingredients.items()
+        }
+
     def coffees_possible_cost(self, price: int):
         """Return the maximum number of coffees you could get for a certain price"""
         return int(price) // self.price
@@ -56,7 +64,11 @@ class CoffeeCreator(Coffee):
         ### Returns:\n
             \t{tuple} -- 0 {int}: Maximum number of coffees - 1 {set}: Limiters
         """
-        amount_of_coffees = {ingredient: self.coffees_possible_ingredient(ingredient, amount) for ingredient, amount in limited_ingredients.items() if ingredient in self.ingredients}
+        amount_of_coffees = {
+            ingredient: self.coffees_possible_ingredient(ingredient, amount)
+            for ingredient, amount in limited_ingredients.items()
+            if ingredient in self.ingredients
+        }
 
         possible_ingredients = {}
         for ingredient, amount in amount_of_coffees.items():
@@ -64,10 +76,3 @@ class CoffeeCreator(Coffee):
         max_possible = min(possible_ingredients.keys())
 
         return (max_possible, possible_ingredients[max_possible])
-
-if __name__ == "__main__":
-    regular = CoffeeCreator('Regular', {'water': 200, 'milk': 20, 'cocoa': 5}, 6)
-    print(regular)
-
-    limiter = regular.limiting_ingredient({'water': 200, 'milk': 20})
-    print(f"The maximum of coffees you can make is {limiter[0]}, which is limited by {' and '.join(limiter[1])}")

82bb40920a9f28da80197a0c6db924cdb380cbea
Laurent Bourgon <laurent.bourgon@outlook.com>
2020-07-02 01:40:02 -0400

    dependencies creator

---
+++ create_dependencies.py
@@ -0,0 +1,28 @@
+import sys
+import os
+import subprocess
+
+# Accède aux fichiers depuis la racine du programme, et non l'endroit du shell
+os.chdir(os.path.realpath(__file__).replace(os.path.basename(__file__), ""))
+
+qrc = subprocess.run(
+    [
+        "pyside2-rcc",
+        "./src/coffee_interface_resources.qrc",
+        "-o",
+        "./src/coffee_interface_resources_rc.py",
+    ]
+)
+
+ui = subprocess.run(
+    [
+        "pyside2-uic",
+        "./src/coffee_interface_ui.ui",
+        "-o",
+        "./src/coffee_interface_ui.py",
+    ]
+)
+
+for cmd in (qrc, ui):
+    if not cmd.returncode:  # Success
+        print(f"[x] {cmd.args[-1]} created")

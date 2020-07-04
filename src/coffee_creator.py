ef3279996553e57e709ee285c47062c984c1a22e
Laurent Bourgon <laurent.bourgon@outlook.com>
2020-07-04 12:56:27 -0400

    added type annotation

--- src/coffee_creator.py
+++ src/coffee_creator.py
@@ -1,5 +1,6 @@
 # coffee_creator.py
 # CREATE A COFFEE
+from typing import Dict, Tuple, Set
 
 
 class Coffee:
@@ -7,18 +8,18 @@ class Coffee:
 
     ### Arguments:\n
         \tname {str} -- Name of the coffee
-        \tingredients {dict} -- Dictionary of the ingredients in format {ingredients: amount}
+        \tingredients {Dict[str, int]} -- Dictionary of the ingredients in format {ingredients: amount}
         \tprice {int} -- Price of the coffee
     """
 
-    def __init__(self, name: str, ingredients: dict, price: int):
-        self.name = str(name)
-        self.ingredients = {
+    def __init__(self, name: str, ingredients: Dict[str, int], price: int):
+        self.name: str = str(name)
+        self.ingredients: Dict[str, int] = {
             ingredient: int(amount)
             for ingredient, amount in ingredients.items()
             if isinstance(amount, int)
         }
-        self.price = int(price)
+        self.price: int = int(price)
 
     def __str__(self) -> str:
         return f"{self.name} - {self.price}$:\t{' | '.join([f'{ingredient} - {amount} mL' for ingredient, amount in self.ingredients.items()])}"
@@ -29,18 +30,18 @@ class CoffeeCreator(Coffee):
 
     ### Arguments:\n
         \tname {str} -- Name of the coffee
-        \tingredients {dict} -- Dictionary of the ingredients in format {ingredient: amount}
+        \tingredients {Dict[str, int]} -- Dictionary of the ingredients in format {ingredient: amount}
         \tprice {int} -- Price of the coffee
     """
 
-    def __init__(self, name: str, ingredients: dict, price: int):
+    def __init__(self, name: str, ingredients: Dict[str, int], price: int):
         super().__init__(name, ingredients, price)
 
     def final_cost(self, number_of_coffees: int) -> int:
         """Return the total price of the number of coffees specified"""
         return self.price * int(number_of_coffees)
 
-    def ingredients_needed(self, number_of_coffees: int) -> dict:
+    def ingredients_needed(self, number_of_coffees: int) -> Dict[str, int]:
         """Return how many ingredients will be needed for the number of coffees specified"""
         return {
             ingredient: amount * int(number_of_coffees)
@@ -55,24 +56,26 @@ class CoffeeCreator(Coffee):
         """Return the maximum number of coffees you could make for one ingredient"""
         return int(amount) // self.ingredients.get(ingredient)
 
-    def limiting_ingredient(self, limited_ingredients: dict) -> tuple:
+    def limiting_ingredient(self, limited_ingredients: Dict[str, int]) -> Tuple[int, Set[str]]:
         """## Return the maximum number of coffees you can make
 
         ### Arguments:\n
-            \tlimitedingredients {dict} -- Dictionary of ingredients in format {ingredient: amount}
+            \tlimitedingredients {Dict[str, int]} -- Dictionary of ingredients in format {ingredient: amount}
 
         ### Returns:\n
-            \t{tuple} -- 0 {int}: Maximum number of coffees - 1 {set}: Limiters
+            \t{Tuple[int, Set[int, str]]} -- 0 {int}: Maximum number of coffees - 1 {set}: Limiters
         """
-        amount_of_coffees = {
+        amount_of_coffees: Dict[str, int] = {
             ingredient: self.coffees_possible_ingredient(ingredient, amount)
             for ingredient, amount in limited_ingredients.items()
             if ingredient in self.ingredients
         }
 
-        possible_ingredients = {}
+        possible_ingredients: Dict[int, Set[str]] = {}
+
         for ingredient, amount in amount_of_coffees.items():
             possible_ingredients.setdefault(amount, set()).update([ingredient])
-        max_possible = min(possible_ingredients.keys())
+
+        max_possible: int = min(possible_ingredients.keys())
 
         return (max_possible, possible_ingredients[max_possible])

1e7ea90293aa1dbdf49f19101e4b547d4b9bb36e
Laurent Bourgon <laurent.bourgon@outlook.com>
2020-07-11 16:07:23 -0400

    added attributes instead of getting them each time

--- CoffeeMachine/ui/IngredientWidgets.py
+++ CoffeeMachine/ui/IngredientWidgets.py
@@ -10,13 +10,12 @@ class IngredientSelector(QLineEdit):
 
     def __init__(self, parent):
         super().__init__(parent)
+        self.word: str = self.objectName().replace("Selector", "")
+        self.status: QLabel = self.parent().findChild(QLabel, f"{self.word}StatusLabel")  # type: ignore
 
     def setSafeText(self, value: int):
-        # word = self.objectName().replace("Selector", "")
-        status: QLabel = self.parent().findChild(QLabel, f"{self.objectName().replace('Selector', '')}StatusLabel")  # type: ignore
         if len(str(value)) > len(str(self.inputMask())):
-            status.setText(STATUS_EXCEEDING)
-
+            self.status.setText(STATUS_EXCEEDING)
         self.setText(str(value))
 
 

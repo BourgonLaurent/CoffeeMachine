07ce76f1bf36fda87542315447957d73721d5132
Laurent Bourgon <laurent.bourgon@outlook.com>
2020-07-16 14:51:57 -0400

    organized imports

--- CoffeeMachine.py
+++ CoffeeMachine.py
@@ -14,8 +14,10 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from CoffeeMachine.coffee_creator import CoffeeCreator
+# Librairies
 import sys
+
+# Project
 from CoffeeMachine import coffee_gui
 
 if __name__ == "__main__":
